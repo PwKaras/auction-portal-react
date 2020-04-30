c848ec6af644ea40e1445b0d1efc4702c23af275
Paweł Karaś <pwkaras@gmail.com>
2020-04-30 23:58:36 +0200

    basket components

---
+++ src/components/ProductInBasket.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const ProductInBasket = () => (
+
+
+    <div className="container">
+        <div className="row">
+            <div className="col-sm">
+                One of three columns
+            </div>
+            <div className="col-sm">
+                One of three columns
+            </div>
+            <div className="col-sm">
+                One of three columns
+            </div>
+            <div className="col-sm">
+                One of three columns
+            </div>
+        </div>
+    </div>
+)
